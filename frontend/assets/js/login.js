f1b90e99aabb8c1eb7513361be3392ee3ca5ed31
lGnyte <grasu.ioan02@gmail.com>
2023-05-21 23:49:01 +0300

    initial fetch test

--- frontend/assets/js/login.js
+++ frontend/assets/js/login.js
@@ -5,16 +5,34 @@ function login(){
   localStorage.clear('auth');
   const username = document.querySelector("form.login input[name='username']").value;
   const password = document.querySelector("form.login input[name='password']").value;
-  console.log(`Username: ${username}`);
-  console.log(`Password: ${password}`);
+
+  fetch("http://localhost:8000/api/v1/login", {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({username, password})
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    if (data.status == "Success") {
+      const token = data.token;
+      localStorage.setItem('token', token);
+    } else if (data.status == "Invalid") {
+      console.log("Invalid username or password");
+    } else {
+      return data;
+    }
+  })
+  .catch(console.error)
+  .finally(() => {
+    //redirect to homepage
+    // window.location.replace('/');
+  });
 
   //save data in localstorage
-  localStorage.setItem('auth', JSON.stringify({username, password}));
-
-  //redirect to homepage
-  window.location.replace('/');
-
-  // return false; //prevents page reload
+  // localStorage.setItem('auth', JSON.stringify({username, password}));
 }
 
 
